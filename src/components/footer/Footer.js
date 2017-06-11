d56aa6586eb7b41247919ac38d4f23744ffad699
Jordan McKinney <jordanmmck@gmail.com>
2017-06-11 09:54:18 -0700

    settings up font awesome

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Footer.css';
-
+import FontAwesome from 'react-fontawesome';
 
 const Footer = () => (
     <div className="footer">
         <a href="https://github.com/jordanmckinney">Github</a>
         <a href="https://twitter.com/JordanMMcKinney">Twitter</a>
+        // <FontAwesome name='rocket' />
     </div>
 )
 
