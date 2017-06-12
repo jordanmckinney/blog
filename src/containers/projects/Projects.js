5b1dae0dd84006454f86d2d97b2b1c764cf53e5b
Jordan McKinney <jordanmmck@gmail.com>
2017-06-11 21:56:29 -0700

    minor tweaks

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -1,42 +1,9 @@
 import React from 'react';
-import {
-  Route,
-  Link
-} from 'react-router-dom'
 import './Projects.css';
 
 
-const Project = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-const Projects = ({ match }) => (
-  <div>
-    <ul>
-      <li>
-        <Link to={`${match.url}/project_1`}>
-          Project 1
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/project_2`}>
-          Project 2
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/project_3`}>
-          Project 3
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Project}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
+const Projects = () => (
+  <div></div>
 )
 
 export default Projects;
