2ebcd340c8cce79f2131906d7b6a8b1aa1e1c7af
Jordan McKinney <jordanmmck@gmail.com>
2017-06-14 22:11:56 -0700

    orange title font, 0.9 opacity article

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -4,7 +4,7 @@ import Article from '../../components/article/Article';
 import Footer from '../../components/footer/Footer';
 import headshot from '../../images/headshot.jpg';
 
-const input = 'I am a 4th year Computer Science student at UVIC. I am currently on an 8 month co-op at Bambora in Victoria.\nMy main interests are in Machine Learning, AI and Blockchain technology.'
+const input = 'I am a 4th year Computer Science student at UVIC. I am currently on an 8 month co-op at Bambora in Victoria.\nMy main interests are in Machine Learning, AI and Blockchain technology.\n\nI am a 4th year Computer Science student at UVIC. I am currently on an 8 month co-op at Bambora in Victoria.\nMy main interests are in Machine Learning, AI and Blockchain technology. I am a 4th year Computer Science student at UVIC. I am currently on an 8 month co-op at Bambora in Victoria.\nMy main interests are in Machine Learning, AI and Blockchain technology.'
 
 const About = () => (
   <div>
