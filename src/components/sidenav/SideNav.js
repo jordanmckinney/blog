1c4bbaf1db0e548f57cef2a33b1b7e2db09f39db
Jordan McKinney <jordanmmck@gmail.com>
2017-06-19 20:40:21 -0700

    grey color scheme, working on sidenav

--- src/components/sidenav/SideNav.js
+++ src/components/sidenav/SideNav.js
@@ -5,22 +5,10 @@ import { Link } from 'react-router-dom'
 
 const SideNav = () => (
     <div className="sidenav">
-        <ul>
-            <li>Bitcoin</li>
-            <li>Ethereum</li>
-            <li>Hash Functions</li>
-            <li>Biology</li>
-            <li>The Human Species</li>
-            <li>Physics</li>
-            <li>Kubrick</li>
-            <li>Musk</li>
-            <li>Feynman</li>
-            <li>Technology</li>
-            <li>Blockchain</li>
-            <li>Ethics</li>
-            <li>Mining Ether</li>
-            <li>Meditation</li>
-        </ul>
+      <li><a href="#">Bitcoin</a></li>
+      <li><a href="#">Ethereum</a></li>
+      <li><a href="#">Hashing</a></li>
+      <li><a href="#">Vim</a></li>
     </div>
 )
 
