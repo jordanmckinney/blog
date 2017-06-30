d73b53f6d78b05e63a9d136f722458debb3d74f8
Jordan McKinney <jordanmmck@gmail.com>
2017-06-29 21:41:15 -0700

    header and footer proper spacing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,11 +3,13 @@ import './Footer.scss';
 
 
 const Footer = () => (
-    <div className="footer">
-        <a href="https://github.com/JordanMcKinney" target="_blank"><i className="fa fa-github fa-lg"></i></a>
-        <a href="https://twitter.com/JordanMMcKinney" target="_blank"><i className="fa fa-twitter fa-lg"></i></a>
-        <a href="https://www.linkedin.com/in/JordanMMcKinney" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a>
-        <a href="mailto@jordanmmck@gmail.com" target="_top"><i className="fa fa-envelope fa-lg"></i></a>
+    <div className="footer-container">
+        <div className="footer">
+            <a href="https://github.com/JordanMcKinney" target="_blank"><i className="fa fa-github fa-lg"></i></a>
+            <a href="https://twitter.com/JordanMMcKinney" target="_blank"><i className="fa fa-twitter fa-lg"></i></a>
+            <a href="https://www.linkedin.com/in/JordanMMcKinney" target="_blank"><i className="fa fa-linkedin fa-lg"></i></a>
+            <a href="mailto@jordanmmck@gmail.com" target="_top"><i className="fa fa-envelope fa-lg"></i></a>
+        </div>
     </div>
 )
 
