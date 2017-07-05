849291d0c6c9d48e23ef2b6a675e81b2b373448b
Jordan McKinney <jordanmmck@gmail.com>
2017-07-04 21:33:40 -0700

    removed icons, added header bar

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -4,27 +4,30 @@ import './Guide.scss';
 
 const Guide = () => (
     <div className="guide-container">
-        <div className="guide">
-            <span className="fa-stack fa-lg">
-              <i className="fa fa-circle fa-stack-2x"></i>
-              <i className="fa fa-hashtag fa-stack-1x"></i>
-            </span>
-            <span className="fa-stack fa-lg">
-              <i className="fa fa-circle fa-stack-2x"></i>
-              <i className="fa fa-link fa-stack-1x"></i>
-            </span>
-            <img src="/public/images/BTC.svg" alt="hash"/>
-            <img src="/public/images/ETH-alt.svg" alt="ethereum"/>
-            <img src="/public/images/DAO-alt.svg" alt="ethereum"/>
-        </div>
+      <div className="guide">
+        <a href="">HASHING</a>
+        <a href="">BLOCKCHAIN</a>
+        <a href="">BITCOIN</a>
+        <a href="">ETHEREUM</a>
+        <a href="">DAO</a>
+        <a href=""></a>
+      </div>
     </div>
 )
 
 
 export default Guide;
 
-            // <a href="https://github.com/JordanMcKinney" target="_blank" rel="noopener noreferrer"><i className="fa fa-hashtag fa-lg"></i></a>
-            // <a href="https://medium.com/@jordanmm" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium fa-lg"></i></a>
-            // <a href="https://twitter.com/JordanMMcKinney" target="_blank" rel="noopener noreferrer"><i className="fa fa-btc fa-lg"></i></a>
-            // <a href="https://www.linkedin.com/in/JordanMMcKinney" target="_blank" rel="noopener noreferrer"><i className="fa fa-eth fa-lg"></i></a>
-            // <a href="mailto@jordanmmck@gmail.com" target="_top"><i className="fa fa-envelope fa-lg"></i></a>
+        // <div className="guide">
+        //     <span className="fa-stack fa-lg">
+        //       <i className="fa fa-circle fa-stack-2x"></i>
+        //       <i className="fa fa-hashtag fa-stack-1x"></i>
+        //     </span>
+        //     <span className="fa-stack fa-lg">
+        //       <i className="fa fa-circle fa-stack-2x"></i>
+        //       <i className="fa fa-link fa-stack-1x"></i>
+        //     </span>
+        //     <img src="/public/images/BTC.svg" alt="hash"/>
+        //     <img src="/public/images/ETH-alt.svg" alt="ethereum"/>
+        //     <img src="/public/images/DAO-alt.svg" alt="ethereum"/>
+        // </div>
