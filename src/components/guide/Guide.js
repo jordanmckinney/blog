a89820e3d5051ed66f8d706218afc981c5cbeb72
Jordan McKinney <jordanmmck@gmail.com>
2017-07-09 11:46:02 -0700

    can't get routing to work properly, hack solution for now

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -5,7 +5,7 @@ import './Guide.scss';
 const Guide = () => (
     <div className="guide-container">
       <div className="guide">
-        <a href="#/writing/hashing" className="guide-link inactive">Hashing</a>
+        <a href="#/writing/" className="guide-link">Hashing</a>
         <a href="#/writing/blockchain" className="guide-link inactive">Blockchain</a>
         <a href="#/writing/bitcoin" className="guide-link inactive">Bitcoin</a>
         <a href="#/writing/ethereum" className="guide-link inactive">Ethereum</a>
