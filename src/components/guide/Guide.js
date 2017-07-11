9a7573a82111b624cbd37811a3fa9e6ddf7412df
Jordan McKinney <jordanmmck@gmail.com>
2017-07-10 23:09:02 -0700

    finished technology post

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -5,7 +5,8 @@ import './Guide.scss';
 const Guide = () => (
     <div className="guide-container">
       <div className="guide">
-        <a href="#/writing/" className="guide-link">Hashing</a>
+        <a href="#/writing/" className="guide-link">Technology</a>
+        <a href="#/writing/hashing" className="guide-link inactive">Hashing</a>
         <a href="#/writing/blockchain" className="guide-link inactive">Blockchain</a>
         <a href="#/writing/bitcoin" className="guide-link inactive">Bitcoin</a>
         <a href="#/writing/ethereum" className="guide-link inactive">Ethereum</a>
--- src/containers/writing/Writing.js
+++ src/containers/writing/Writing.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Article from '../../components/article/Article';
 import Guide from '../../components/guide/Guide';
 import Hashing from '../../documents/posts/Hashing.md';
+import Technology from '../../documents/posts/Technology.md';
 // import { Route, Link } from 'react-router-dom'
 
 
 const Writing = () => (
   <div className="writing-container">
       <Guide />
-      <Article source={Hashing}/>
+      <Article source={Technology}/>
   </div>
 )
 
