700c71e1e7649b667e4910389077dcfff6968493
Jordan McKinney <jordanmmck@gmail.com>
2017-07-26 08:08:11 -0700

    removed separate blockchain post

--- src/components/guide/Guide.js
+++ src/components/guide/Guide.js
@@ -5,8 +5,7 @@ import './Guide.scss';
 const Guide = () => (
     <div className="guide-container">
       <div className="guide">
-        <a href="#/writing/" className="guide-link">Tech</a>
-        <a href="#/writing/blockchain" className="guide-link inactive">Blockchain</a>
+        <a href="#/writing/" className="guide-link">Technology</a>
         <a href="#/writing/bitcoin" className="guide-link inactive">Bitcoin</a>
         <a href="#/writing/ethereum" className="guide-link inactive">Ethereum</a>
         <a href="#/writing/dao" className="guide-link inactive">DAO</a>
