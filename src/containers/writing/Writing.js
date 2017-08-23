dedb38bddb6195f793bc1e7e5875299e8954cda0
Jordan McKinney <jordanmmck@gmail.com>
2017-08-22 22:13:12 -0700

    new build

--- src/containers/writing/Writing.js
+++ src/containers/writing/Writing.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Article from '../../components/article/Article';
 import Guide from '../../components/guide/Guide';
-import Money from '../../documents/posts/Money.md';
+import Technology from '../../documents/posts/Technology.md';
 // import { Route, Link } from 'react-router-dom'
 
 
 const Writing = () => (
   <div className="writing-container">
       <Guide />
-      <Article source={Money}/>
+      <Article source={Technology}/>
   </div>
 )
 
