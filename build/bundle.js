5d6e1908ad2125f7252fa759aba1f6959e6fa870
Jordan McKinney <jordanmmck@gmail.com>
2017-08-26 13:26:32 -0700

    rewording again

--- build/bundle.js
+++ build/bundle.js
@@ -21538,7 +21538,7 @@ module.exports = "[![The Cryptoeconomic Way, Vitalik Buterin](http://img.youtube
 /* 188 */
 /***/ (function(module, exports) {
 
-module.exports = "[![Fun to Imagine](http://img.youtube.com/vi/v3pYRn5j7oI/0.jpg)](https://youtu.be/v3pYRn5j7oI?list=PL2D30B1DEFFDA0310 \"Fun to Imagine\")\n\n<p id=\"title\">Fun to Imagine, Richard Feynman</p>\n\nI cannot overstate how impactful these videos have been to my life. I found them several years ago and, along with Feynman's other work, they ignited in me a new appreciation for math, science and nature. Feynman's way of thinking along with Elon Musk's incredible drive and vision were the key factors which inpsired me to go back to school for engineering at 26.\n\n"
+module.exports = "[![Fun to Imagine](http://img.youtube.com/vi/v3pYRn5j7oI/0.jpg)](https://youtu.be/v3pYRn5j7oI?list=PL2D30B1DEFFDA0310 \"Fun to Imagine\")\n\n<p id=\"title\">Fun to Imagine, Richard Feynman</p>\n\nI cannot overstate how impactful these videos have been to my life. I found them several years ago and, along with Feynman's other work, they ignited in me a new appreciation for math, science and nature. Feynman's way of thinking along with Elon Musk's incredible drive and vision were the key factors which inpsired me to re-learn math and science and go back to school.\n\n"
 
 /***/ }),
 /* 189 */
--- src/documents/videos/funtoimagine.md
+++ src/documents/videos/funtoimagine.md
@@ -2,5 +2,5 @@
 
 <p id="title">Fun to Imagine, Richard Feynman</p>
 
-I cannot overstate how impactful these videos have been to my life. I found them several years ago and, along with Feynman's other work, they ignited in me a new appreciation for math, science and nature. Feynman's way of thinking along with Elon Musk's incredible drive and vision were the key factors which inpsired me to go back to school for engineering at 26.
+I cannot overstate how impactful these videos have been to my life. I found them several years ago and, along with Feynman's other work, they ignited in me a new appreciation for math, science and nature. Feynman's way of thinking along with Elon Musk's incredible drive and vision were the key factors which inpsired me to re-learn math and science and go back to school.
 
