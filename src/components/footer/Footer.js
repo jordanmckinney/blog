705c9a643042401486791a7e3dafb280f905a776
Jordan McKinney <jordanmmck@gmail.com>
2017-09-05 20:40:59 -0700

    added hover text to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,11 +5,11 @@ import './Footer.scss';
 const Footer = () => (
     <div className="footer-container">
         <div className="footer">
-            <a href="https://github.com/jordanmmck" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-lg"></i></a>
-            <a href="https://medium.com/@jordanmmck" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium fa-lg"></i></a>
-            <a href="https://www.linkedin.com/in/JordanMMcKinney" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin fa-lg"></i></a>
-            <a href="mailto@jordanmmck@gmail.com" target="_top"><i className="fa fa-envelope fa-lg"></i></a>
-            <a href="https://docs.google.com/document/d/1zD1RgyvYjjzkKQyvTt18CCDywk88Gdy9Ulc77cNjD4k/edit?usp=sharing" target="_top"><i className="fa fa-file-text fa-lg"></i></a>
+            <a href="https://github.com/jordanmmck" title="github" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-lg"></i></a>
+            <a href="https://medium.com/@jordanmmck" title="medium" target="_blank" rel="noopener noreferrer"><i className="fa fa-medium fa-lg"></i></a>
+            <a href="https://www.linkedin.com/in/JordanMMcKinney" title="linkedin" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin fa-lg"></i></a>
+            <a href="mailto@jordanmmck@gmail.com" title="email" target="_top"><i className="fa fa-envelope fa-lg"></i></a>
+            <a href="https://docs.google.com/document/d/1zD1RgyvYjjzkKQyvTt18CCDywk88Gdy9Ulc77cNjD4k/edit?usp=sharing" title="resume" target="_top"><i className="fa fa-file-text fa-lg"></i></a>
         </div>
     </div>
 )
