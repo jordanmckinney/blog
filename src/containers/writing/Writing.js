5f667e2fa11c29ceb4508af559b73b3612f68ee4
Jordan McKinney <jordanmmck@gmail.com>
2017-09-13 18:11:30 -0700

    added cryptolife post

--- src/containers/writing/Writing.js
+++ src/containers/writing/Writing.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Article from '../../components/article/Article';
+import CryptoLife from '../../documents/posts/medium/cryptolife.md';
 import MoneyPost from '../../documents/posts/medium/money.md';
 import BlockchainNotes from '../../documents/posts/BlockchainNotes.md';
 
 
 const Writing = () => (
   <div className="writing-container">
+    <Article source={CryptoLife}/>
     <Article source={MoneyPost}/>
     <Article source={BlockchainNotes}/>
   </div>
---
+++ src/documents/posts/medium/cryptolife.md
@@ -0,0 +1,5 @@
+<center> <h2>Cryptocurrencies as Abstract Lifeforms</h2> </center>
+
+[Medium post](https://medium.com/@jordanmmck/markets-money-bitcoin-96e5539bceef) on cryptoeconomic systems and how they grow, evolve, and speciate like organisms, as well as Ethereum smart contracts and how they function like abstract machines or automatons.
+
+<img src="/public/images/fractal.jpg" alt="fractal"/>
