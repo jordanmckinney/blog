6bea37034f3f972303db0b6b2b8683a86236d888
Jordan McKinney <jordanmmck@gmail.com>
2018-07-23 09:38:12 -0700

    about me

--- build/bundle.js
+++ build/bundle.js
@@ -21616,7 +21616,7 @@ module.exports = "<img src=\"../../public/images/book_covers/zerotoone.jpg\" id=
 /* 190 */
 /***/ (function(module, exports) {
 
-module.exports = "<center><h2>About Me</h2></center>\n\n<img src=\"/public/images/headshot.jpg\" id=\"headshot\" alt=\"headshot\"/>\n\nI'm a 4th year Computer Science student at UVIC.\nI've worked for 1 year as a web developer co-op (4 months at Redbrick, 8 months at Bambora).\n\nMy main interest is in cryptocurrencies.\nI write blog posts and make YouTube videos on the subject of cryptocurrencies, and I run a Slack channel called VicBlockchain where we discuss all things crypto, organize meetups, and so on.\n\nMy goal once finished school is to move into the finance and investing side of crypto.\n"
+module.exports = "<center><h2>About Me</h2></center>\n\n<img src=\"/public/images/headshot.jpg\" id=\"headshot\" alt=\"headshot\"/>\n\nI'm a 4th year Computer Science student at UVIC.\nI've worked for 1 year as a web developer co-op (4 months at Redbrick, 8 months at Bambora).\n\nI write blog posts and make YouTube videos on the subject of cryptocurrencies, and I run a Slack channel called VicBlockchain where we discuss all things crypto, organize meetups, and so on.\n\nMy goal once finished school is to move into the finance and investing side of crypto.\n"
 
 /***/ }),
 /* 191 */
--- src/documents/posts/AboutMe.md
+++ src/documents/posts/AboutMe.md
@@ -5,7 +5,6 @@
 I'm a 4th year Computer Science student at UVIC.
 I've worked for 1 year as a web developer co-op (4 months at Redbrick, 8 months at Bambora).
 
-My main interest is in cryptocurrencies.
 I write blog posts and make YouTube videos on the subject of cryptocurrencies, and I run a Slack channel called VicBlockchain where we discuss all things crypto, organize meetups, and so on.
 
 My goal once finished school is to move into the finance and investing side of crypto.
