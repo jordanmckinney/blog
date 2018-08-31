65f464065dae48de86045a048daf4e5e050d9c26
Jordan McKinney <jordanmmck@gmail.com>
2018-08-31 12:27:00 -0700

    added coinmonks link etc

--- build/bundle.js
+++ build/bundle.js
@@ -20589,7 +20589,7 @@ module.exports = "<center><h2>About Me</h2></center>\n\n<img src=\"/public/image
 /* 198 */
 /***/ (function(module, exports) {
 
-module.exports = "<center> <h2>Bitcoin Security: a Negative Exponential</h2> </center>\n\n[Medium post](https://medium.com/@jordanmmck/bitcoin-security-a-negative-exponential-95e78b6b575) on the consequences of Bitcoin's declining block reward. I argue that under proof-of-work, and given the current supply schedule, Bitcoin security will decline exponentially until it dies. [Tweetstorm](https://twitter.com/jordanmmck/status/1034834965127557120).\n\n<img src=\"/public/images/btc_post_cover.jpg\" alt=\"rocks\"/>\n"
+module.exports = "<center> <h2>Bitcoin Security: a Negative Exponential</h2> </center>\n\n[Medium post](https://medium.com/@jordanmmck/bitcoin-security-a-negative-exponential-95e78b6b575) on the consequences of Bitcoin's declining block reward. I argue that under proof-of-work, and given the current supply schedule, Bitcoin security will decline exponentially until it dies. \n\nThe post was added to the [Coinmonks](https://medium.com/coinmonks) Medium publication, and was republished on [nakedtruth.in](http://www.nakedtruth.in/2018/08/31/the-unsolved-flaw-in-bitcoin/). I also summarized the post via [Tweetstorm](https://twitter.com/jordanmmck/status/1034834965127557120).\n\n<img src=\"/public/images/btc_post_cover.jpg\" alt=\"rocks\"/>\n"
 
 /***/ }),
 /* 199 */
--- src/documents/posts/medium/bitcoin.md
+++ src/documents/posts/medium/bitcoin.md
@@ -1,5 +1,7 @@
 <center> <h2>Bitcoin Security: a Negative Exponential</h2> </center>
 
-[Medium post](https://medium.com/@jordanmmck/bitcoin-security-a-negative-exponential-95e78b6b575) on the consequences of Bitcoin's declining block reward. I argue that under proof-of-work, and given the current supply schedule, Bitcoin security will decline exponentially until it dies. [Tweetstorm](https://twitter.com/jordanmmck/status/1034834965127557120).
+[Medium post](https://medium.com/@jordanmmck/bitcoin-security-a-negative-exponential-95e78b6b575) on the consequences of Bitcoin's declining block reward. I argue that under proof-of-work, and given the current supply schedule, Bitcoin security will decline exponentially until it dies. 
+
+The post was added to the [Coinmonks](https://medium.com/coinmonks) Medium publication, and was republished on [nakedtruth.in](http://www.nakedtruth.in/2018/08/31/the-unsolved-flaw-in-bitcoin/). I also summarized the post via [Tweetstorm](https://twitter.com/jordanmmck/status/1034834965127557120).
 
 <img src="/public/images/btc_post_cover.jpg" alt="rocks"/>
