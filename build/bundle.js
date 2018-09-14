42d967f653ed9f99969104c32f233c4acf9b4696
Jordan McKinney <jordanmmck@gmail.com>
2018-09-14 11:22:45 -0700

    compressed images

--- build/bundle.js
+++ build/bundle.js
@@ -20632,7 +20632,7 @@ module.exports = "[![EOS Explained (and Criticized)](http://img.youtube.com/vi/c
 /* 212 */
 /***/ (function(module, exports) {
 
-module.exports = "[![Membran Labs Ep 2](/public/images/podcast_1_med.jpg)](https://soundcloud.com/tyler-harnadek-686048240/episode-2-july-20-2018-with-jordan-mckinney)\n\n<p id=\"title\">Podcast: Membran Labs Ep. 2</p>\n\nI was a guest on the [Membran Labs Podcast](https://soundcloud.com/tyler-harnadek-686048240/episode-2-july-20-2018-with-jordan-mckinney) with Tyler Harndek.\nWe talked about a possible attack on Ethereum, EOS, decentralized gaming, and Augur.\n\n"
+module.exports = "[![Membran Labs Ep 2](/public/images/podcast_1_wide.jpg)](https://soundcloud.com/tyler-harnadek-686048240/episode-2-july-20-2018-with-jordan-mckinney)\n\n<p id=\"title\">Podcast: Membran Labs Ep. 2</p>\n\nI was a guest on the [Membran Labs Podcast](https://soundcloud.com/tyler-harnadek-686048240/episode-2-july-20-2018-with-jordan-mckinney) with Tyler Harndek.\nWe talked about a possible attack on Ethereum, EOS, decentralized gaming, and Augur.\n\n"
 
 /***/ }),
 /* 213 */
