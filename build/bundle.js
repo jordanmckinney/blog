0236ef2578916cd763338877cbe452b355fcf534
Jordan McKinney <jordanmmck@gmail.com>
2018-09-24 17:09:12 -0700

    added cryptochat link

--- build/bundle.js
+++ build/bundle.js
@@ -20448,7 +20448,7 @@ module.exports = "<center> <p id=\"title\">Bitcoin Security: a Negative Exponent
 /* 191 */
 /***/ (function(module, exports) {
 
-module.exports = "<center> <p id=\"title\">Bitcoin Security in One Chart</p> </center>\n\n[Medium post](https://medium.com/coinmonks/bitcoin-security-in-one-chart-694ee3ed8c2d) plotting Bitcoin \"security factor\" over time, and extrapolating into the future.\n\nThis post, like the previous one, was added to the [Coinmonks](https://medium.com/coinmonks) Medium publication. \nI also summarized the post via [Tweetstorm](https://twitter.com/jordanmmck/status/1042074859977592833).\n\n<img src=\"/public/images/security_factor.png\" alt=\"security factor chart\"/>\n"
+module.exports = "<center> <p id=\"title\">Bitcoin Security in One Chart</p> </center>\n\n[Medium post](https://medium.com/coinmonks/bitcoin-security-in-one-chart-694ee3ed8c2d) plotting Bitcoin \"security factor\" over time, and extrapolating into the future.\n\nThis post, like the previous one, was added to the [Coinmonks](https://medium.com/coinmonks) Medium publication, and it was featured in the [CryptoChat](https://us16.campaign-archive.com/?u=1f5a2ff3f60a10d68aea34a25&id=2344913960) newsletter. \nI also summarized the post via [Tweetstorm](https://twitter.com/jordanmmck/status/1042074859977592833).\n\n<img src=\"/public/images/security_factor.png\" alt=\"security factor chart\"/>\n"
 
 /***/ }),
 /* 192 */
--- src/documents/posts/medium/bitcoin_II.md
+++ src/documents/posts/medium/bitcoin_II.md
@@ -2,7 +2,7 @@
 
 [Medium post](https://medium.com/coinmonks/bitcoin-security-in-one-chart-694ee3ed8c2d) plotting Bitcoin "security factor" over time, and extrapolating into the future.
 
-This post, like the previous one, was added to the [Coinmonks](https://medium.com/coinmonks) Medium publication. 
+This post, like the previous one, was added to the [Coinmonks](https://medium.com/coinmonks) Medium publication, and it was featured in the [CryptoChat](https://us16.campaign-archive.com/?u=1f5a2ff3f60a10d68aea34a25&id=2344913960) newsletter. 
 I also summarized the post via [Tweetstorm](https://twitter.com/jordanmmck/status/1042074859977592833).
 
 <img src="/public/images/security_factor.png" alt="security factor chart"/>
