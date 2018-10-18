bc373be242b633baa133a3dd1e078e5aa1b905a0
Jordan McKinney <jordanmmck@gmail.com>
2018-10-18 06:15:46 -0700

    store

--- build/bundle.js
+++ build/bundle.js
@@ -20447,7 +20447,7 @@ module.exports = "<img src=\"../../public/images/book_covers/zerotoone.jpg\" id=
 /* 190 */
 /***/ (function(module, exports) {
 
-module.exports = "<img src=\"/public/images/podcast_1_close.jpg\" id=\"headshot\" alt=\"headshot\"/>\n\nI'm a 4th year Computer Science student at UVIC.\nI've worked for 1 year as a web developer co-op (4 months at [Redbrick](https://rdbrck.com/), 8 months at [Bambora](https://www.bambora.com/en/us/)).\n\nI write [blog posts](https://medium.com/@jordanmmck) and make [YouTube videos](https://www.youtube.com/user/Brock34Landers) on the subject of cryptocurrencies, and I run a [Slack channel](https://vicblockchain.now.sh/) called VicBlockchain where we discuss all things crypto, organize meetups, etc.\n\nMy goal after graduating this December is to work as an analyst at a crypto investment fund.\nI can be reached at [jordanmmck@gmail.com](mailto:jordanmmck@gmail.com).\n"
+module.exports = "<img src=\"/public/images/podcast_1_close.jpg\" id=\"headshot\" alt=\"headshot\"/>\n\nI'm a 4th year Computer Science student at UVIC, and part-time researcher at [Storecoin](http://storeco.in/).\nI've done web development coops at [Redbrick](https://rdbrck.com/) and [Bambora](https://www.bambora.com/en/us/).\n\nI write [blog posts](https://medium.com/@jordanmmck) and make [YouTube videos](https://www.youtube.com/user/Brock34Landers) on the subject of cryptocurrencies, and I run a [Slack channel](https://vicblockchain.now.sh/) called VicBlockchain where we discuss all things crypto, organize meetups, etc.\n\nAfter graduating in December I hope to work as an analyst at a crypto investment fund.\nI can be reached at [jordanmmck@gmail.com](mailto:jordanmmck@gmail.com).\n"
 
 /***/ }),
 /* 191 */
--- src/documents/posts/AboutMe.md
+++ src/documents/posts/AboutMe.md
@@ -1,9 +1,9 @@
 <img src="/public/images/podcast_1_close.jpg" id="headshot" alt="headshot"/>
 
-I'm a 4th year Computer Science student at UVIC.
-I've worked for 1 year as a web developer co-op (4 months at [Redbrick](https://rdbrck.com/), 8 months at [Bambora](https://www.bambora.com/en/us/)).
+I'm a 4th year Computer Science student at UVIC, and part-time researcher at [Storecoin](http://storeco.in/).
+I've done web development coops at [Redbrick](https://rdbrck.com/) and [Bambora](https://www.bambora.com/en/us/).
 
 I write [blog posts](https://medium.com/@jordanmmck) and make [YouTube videos](https://www.youtube.com/user/Brock34Landers) on the subject of cryptocurrencies, and I run a [Slack channel](https://vicblockchain.now.sh/) called VicBlockchain where we discuss all things crypto, organize meetups, etc.
 
-My goal after graduating this December is to work as an analyst at a crypto investment fund.
+After graduating in December I hope to work as an analyst at a crypto investment fund.
 I can be reached at [jordanmmck@gmail.com](mailto:jordanmmck@gmail.com).
